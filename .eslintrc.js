9af640781de73a5c2d404114707baeb8e9d7b60e
ganpeng <190875565@qq.com>
2022-01-21 23:06:58 +0800

    chore: 升级依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
+    'vue/setup-compiler-macros': true,
   },
   extends: ['plugin:vue/vue3-recommended', 'prettier', 'plugin:prettier/recommended'],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
   plugins: ['vue', '@typescript-eslint'],
-  rules: {},
+  rules: {
+    // vue
+    'vue/multi-word-component-names': 'off',
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      spacing: {
-        12.5: '3.125rem',
-        13: '3.25rem',
-        15: '3.75rem',
-        52.5: '12.125rem',
-        128: '32rem',
-        144: '36rem',
-      },
-    },
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
