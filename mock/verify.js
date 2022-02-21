4dbe19593ec7e4be57d5e39967885fbc706ba4fd
gp <190875565@qq.com>
2022-02-21 15:02:30 +0800

    fix: mock权限

--- mock/verify.js
+++ mock/verify.js
@@ -4,9 +4,13 @@ const SECRET_KEY = '123456789'
 const verifyToken = (token) => {
   return jwt.verify(token, SECRET_KEY)
 }
+const urls = [/\/users\/[0-9]+/, /\/permission_routes/]
+function isUrlAllowed(url) {
+  return urls.some((urlRegex) => urlRegex.test(url))
+}
+
 module.exports = function (req, res, next) {
-  const urls = ['/users', '/permission_routes']
-  if (urls.includes(req.url)) {
+  if (isUrlAllowed(req.url)) {
     const token = req.headers.authorization
     if (!token) {
       res.status(401).jsonp({ code: 401, msg: '请先登录' })
