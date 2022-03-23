d44a2ab71e063ca447ab0b8fde76a7496e6a979b
gp <190875565@qq.com>
2022-03-23 17:36:35 +0800

    feat: echarts

---
+++ src/pages/dashboard/components/VisitAnalysis.vue
@@ -0,0 +1,14 @@
+<template>
+  <div ref="chartRef"></div>
+</template>
+
+<script setup lang="ts">
+  import { useECharts } from '@/hooks/useECharts'
+  import type { Ref } from 'vue'
+  import { ref } from 'vue'
+
+  const chartRef = ref<HTMLDivElement | null>(null)
+  useECharts(chartRef as Ref<HTMLDivElement>)
+</script>
+
+<style lang="less"></style>
